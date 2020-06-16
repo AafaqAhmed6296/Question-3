d25464b25f22d1c98f8f903c42d12c8c343b9c35
Afaq Ahmed Saqi <afaqahmad6296@gmail.com>
2020-06-17 00:03:02 +0500

    Store data in structs.

--- Question#3/main.cpp
+++ Question#3/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+using namespace std;
 
 struct Player {
     char playerName[20];
@@ -28,5 +29,27 @@ struct Team {
 int main(){
     
     Team team[5];
+    
+    for (int i = 0; i < 5 ; i++) {
+        cout << "Enter Team " << i+1 << " Id "; cin >> team[i].teamId;
+        cout << "Enter Team " << i+1 << " name "; cin >> team[i].teamName;
+        cout << "Enter Team " << i+1 << " matchesPlayed: "; cin >> team[i].matchesPlayed;
+        cout << "Enter Team " << i+1 << " win matches: "; cin >> team[i].winCount;
+        cout << "Enter Team " << i+1 << " draw matches :"; cin >> team[i].drawCount;
+        cout << "Enter Team " << i+1 << " lossCount: "; cin >> team[i].lossCount;
+        for (int k = 0 ; k < 11 ; k++) {
+            cout << "Enter Team " << i+1 << " player's Name: "; cin >>  team[i].player[k].playerName;
+            cout << "Enter Team " << i+1 << " player's runs: "; cin >>  team[i].player[k].runs;
+            cout << "Enter Team " << i+1 << " player's innings: "; cin >>  team[i].player[k].innings;
+            cout << "Enter Team " << i+1 << " player's times not out: "; cin >>  team[i].player[k].notOut;
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
     return 0;
 }
